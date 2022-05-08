5eefa862d02151b10c608dd3793d9b82c278010b
Z0E <shotarotakazoe2000@gmail.com>
2022-05-08 12:05:45 +0000

    [AC] abc250 abc250_a

---
+++ atcoder.jp/abc250/abc250_a/Main.py
@@ -0,0 +1,17 @@
+# in
+H, W = map(int, input().split())
+R, C = map(int, input().split())
+
+# calc
+count = 0
+if R > 1:
+  count += 1
+if R < H:
+  count += 1
+if C > 1:
+  count += 1
+if C < W:
+  count += 1
+
+# out
+print(count)
