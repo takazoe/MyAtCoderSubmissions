aeb7d59480e44429bd9fd38526e912aba14f9551
Z0E <shotarotakazoe2000@gmail.com>
2022-09-03 12:02:20 +0000

    [AC] abc267 abc267_a

---
+++ atcoder.jp/abc267/abc267_a/Main.py
@@ -0,0 +1,14 @@
+# in
+s = input()
+
+# calc
+if s == "Monday":
+  print(5)
+elif s == "Tuesday":
+  print(4)
+elif s == "Wednesday":
+  print(3)
+elif s == "Thursday":
+  print(2)
+else:
+  print(1)
